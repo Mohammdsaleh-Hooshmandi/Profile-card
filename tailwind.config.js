5d5c368ee04e560a5c07a59c0b0dc5428f0efa40
Mohammdsaleh Hooshmandi <Mohammdsale.Hooshmandi1382@gmail.com>
2023-08-23 13:13:41 +0430

    add colors and fonts to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Dark-cyan': 'hsl(185, 75%, 39%)',
+        'Very-dark-desaturated-blue': 'hsl(229, 23%, 23%)',
+        'Dark-grayish-blue': 'hsl(227, 10%, 46%)',
+        'Dark-gray': 'hsl(0, 0%, 59%)',
+      },
+    },
+    fontFamily: {
+      'sans': ['Kumbh Sans', 'sans-serif']
+    }
   },
   plugins: [],
 }
